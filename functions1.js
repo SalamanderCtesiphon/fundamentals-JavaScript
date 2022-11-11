c7c376fc7edabff697e2c60d15969fa1bdbd4872
SalamanderCtesiphon <swbrookshire@gmail.com>
2022-11-11 11:20:49 -0600

    created more files to work the problems

---
+++ functions1.js
@@ -0,0 +1,13 @@
+const names = ['Chris', 'Li Kang', 'Anne', 'Francesca', 'Mustafa', 'Tina', 'Bert', 'Jada']
+const para = document.createElement('p');
+
+// Add your code here
+
+function chooseName() {
+
+
+// Don't edit the code below here!
+
+section.innerHTML = ' ';
+
+section.appendChild(para);
